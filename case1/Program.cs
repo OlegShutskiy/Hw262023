d26577191ea24bb6c9c6af4711a797029f81b28d
Oleg <shuka.21@mail.ru>
2023-02-26 17:13:31 +0300

    homework

---
+++ case1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.Write("Введите пятизначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+bool b = true;
+String text = Convert.ToString(a);
+for (int i = 0; i < 2; i++)
+{
+    if (text[i] == text[text.Length - i - 1])
+    {
+        b = true;
+    }
+    else
+    {
+        b = false;
+    }
+}
+if (b)
+    Console.WriteLine($"число {a} - палиндром");
+else
+    Console.WriteLine($"Число {a} не - полиндром");
+
---
+++ case2/Program.cs
@@ -0,0 +1,23 @@
+﻿int aX, aY, aZ;
+int bX, bY, bZ;
+double resultDoub;
+
+Console.WriteLine("Введите посследовательнсть координат");
+Console.Write("aX: ");
+aX = Convert.ToInt32(Console.ReadLine());
+Console.Write("aY: ");
+aY = Convert.ToInt32(Console.ReadLine());
+Console.Write("aZ: ");
+aZ = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("bX: ");
+bX = Convert.ToInt32(Console.ReadLine());
+Console.Write("bY: ");
+bY = Convert.ToInt32(Console.ReadLine());
+Console.Write("bZ: ");
+bZ = Convert.ToInt32(Console.ReadLine());
+
+resultDoub = Math.Pow(bX - aX, 2) + Math.Pow(bY - aY, 2) + Math.Pow(bZ - aZ, 2);
+resultDoub = Math.Sqrt(resultDoub);
+resultDoub = Math.Round(resultDoub, 2);
+Console.WriteLine(resultDoub);
